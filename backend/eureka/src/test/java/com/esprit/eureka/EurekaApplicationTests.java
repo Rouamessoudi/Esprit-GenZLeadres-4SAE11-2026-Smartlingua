943a96c91425fca49bbe5f037ac31823b5514476
Roua Messaoudi <rouamessaoudi90@gmail.com>
2026-04-29 19:57:26 +0100

    Fix eureka test context loading and add surefire failure logs in CI

--- backend/eureka/src/test/java/com/esprit/eureka/EurekaApplicationTests.java
+++ backend/eureka/src/test/java/com/esprit/eureka/EurekaApplicationTests.java
@@ -1,9 +1,10 @@
 package com.esprit.eureka;
 
+import esprit.eureka.EurekaApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = EurekaApplication.class)
 class EurekaApplicationTests {
 
     @Test
